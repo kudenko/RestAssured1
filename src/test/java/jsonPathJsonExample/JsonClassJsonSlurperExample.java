b1e953e86a0c4529b31561e5c7f825d53db1fb08
Grey <kysese@gmail.com>
2020-07-22 07:10:01 +0300

    added another test

--- src/test/java/jsonPathJsonExample/JsonClassJsonSlurperExample.java
+++ src/test/java/jsonPathJsonExample/JsonClassJsonSlurperExample.java
@@ -4,6 +4,9 @@ import io.restassured.RestAssured;
 import io.restassured.response.ValidatableResponse;
 import org.junit.jupiter.api.*;
 
+import java.util.List;
+import java.util.Map;
+
 public class JsonClassJsonSlurperExample {
 
     static ValidatableResponse validatableResponse;
@@ -53,11 +56,55 @@ public class JsonClassJsonSlurperExample {
         System.out.println("sName = " + sName );
     }
 
-    @DisplayName("Get all the info    ")
+    @DisplayName("Get all the info  of store with ZIP ")
     @Test
-    public void getFirstServiceName2(){
-        String sName = validatableResponse.extract().path("data[0].services[0].name");
-        System.out.println("sName = " + sName );
+    public void findStoreWithZip(){
+        Map<String, ?> info =  validatableResponse.extract().path("data.find{it.zip=='55901'}");
+        System.out.println("info = " + info);
+    }
+
+    @DisplayName("Get address of store with ZIP 55901")
+    @Test
+    public void getAddressFromZipId(){
+        String  info =  validatableResponse.extract().path("data.find{it.zip=='55901'}.address");
+        System.out.println("info = " + info);
+    }
+
+    @DisplayName("Get info from max and min id")
+    @Test
+    public void getAllInfoWithMaxAndMinId(){
+        Map<String, ?> info1 =  validatableResponse.extract().path("data.min{it.id}");
+        Map<String, ?> info =  validatableResponse.extract().path("data.max{it.id}");
+
+        System.out.println("info = " + info);
+        System.out.println("info1 = " + info1);
+
+    }
+    @DisplayName("Get info from max and min id or others")
+    @Test
+    public void getAllInfoWitRangeId(){
+        List<String> info =  validatableResponse.extract().path("data.findAll{it.id<10}.zip");
+
+        System.out.println("info = " + info);
+
+    }
+
+    @DisplayName("Get service name for all the stores")
+    @Test
+    public void getServiceName(){
+        List<String> info =  validatableResponse.extract().path("data.services.findAll{it.name}.name");
+
+        System.out.println("info = " + info);
+
+    }
+
+    @DisplayName("Get service name for the stores with id < 5")
+    @Test
+    public void getServiceNameWithLowerFiveId(){
+        List<List<String>> info =  validatableResponse.extract().path("data.services.findAll{it.name}.name");
+
+        System.out.println("info = " + info);
+
     }
 
 }
